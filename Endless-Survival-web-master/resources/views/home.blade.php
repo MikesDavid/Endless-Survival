050ddf97ceaf6e2a97e95f8eb6bd8ce275fd1e66
MikesDavid <davidmikes87@gmail.com>
2023-05-14 15:52:41 +0200

    Update home.blade.php

--- Endless-Survival-web-master/resources/views/home.blade.php
+++ Endless-Survival-web-master/resources/views/home.blade.php
@@ -6,17 +6,13 @@
             <p class="fs-5">
                 Az Endless Survival egy egyszemélyes túlélő shooter játék (jövőben többjátékos opció is tervben van). A játék során a játékos minden pálya előtt kiválaszthat egy elsődleges és egy másodlagos fegyvert. A játékosok minden pálya után tapasztalati pontot kapnak elegendő tapasztalati pont összegyűjtése esetén a játékos szintet lép és kap egy képesség pontot. A képesség pontokat pályán kívül lehet elkölteni különféle karakter erősítésekre vagy képességekre. Képesség pontok mellé bizonyos szintek elérésekor új fegyverek is elérhetővé válnak.
             </p>
-            <p class="fs-5">A játék célja, hogy a játékos minél tovább próbáljon meg túlélni a folyamatosan erősödő és egyre gyorsabban termőellenségekkel szemben.</p>
             <p class="fs-5">A játéknak Windows operációs rendszer alatt futó, c# nyelven írt asztali alkalmazás. A játék a Unity engine-t használja.</p>
             <br>
             <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
+            <p class="fs-5">Az Endless Survival egy egyszemélyes túlélő shooter játék (jövőben többjátékos opció is tervben van). A játék során a játékos minden pálya előtt kiválaszthat egy elsődleges és egy másodlagos fegyvert. A játékosok minden pálya után tapasztalati pontot kapnak elegendő tapasztalati pont összegyűjtése esetén a játékos szintet lép és kap egy képesség pontot. A képesség pontokat pályán kívül lehet elkölteni különféle karakter erősítésekre vagy képességekre. Képesség pontok mellé bizonyos szintek elérésekor új fegyverek is elérhetővé válnak.
+                A játék célja, hogy a játékos minél tovább próbáljon meg túlélni a folyamatosan erősödő és egyre gyorsabban termőellenségekkel szemben.
+                </p>
+            <p style="text-align:center;"><a target="_blank" href="https://github.com/MikesDavid/Game">Letöltés</a></p>
             <br>
             <br>
             <br>
