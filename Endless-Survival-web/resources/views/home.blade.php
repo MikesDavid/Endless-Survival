82d80df017480b217ea3a7f5b976fde2fd9c4ba0
DomokGx <domok.davidka@gmail.com>
2023-05-19 18:29:26 +0200

    Translate home.blade.php text

--- Endless-Survival-web/resources/views/home.blade.php
+++ Endless-Survival-web/resources/views/home.blade.php
@@ -2,24 +2,12 @@
 @section('content')
 
         <div class="container pb-3">
-            <h1 class="text-center display-6 py-3">A játékról</h1>
-            <p class="fs-5">
-                Az Endless Survival egy egyszemélyes túlélő shooter játék (jövőben többjátékos opció is tervben van). A játék során a játékos minden pálya előtt kiválaszthat egy elsődleges és egy másodlagos fegyvert. A játékosok minden pálya után tapasztalati pontot kapnak elegendő tapasztalati pont összegyűjtése esetén a játékos szintet lép és kap egy képesség pontot. A képesség pontokat pályán kívül lehet elkölteni különféle karakter erősítésekre vagy képességekre. Képesség pontok mellé bizonyos szintek elérésekor új fegyverek is elérhetővé válnak.
-            </p>
-            <p class="fs-5">A játéknak Windows operációs rendszer alatt futó, c# nyelven írt asztali alkalmazás. A játék a Unity engine-t használja.</p>
+            <h1 class="text-center display-6 py-3">{{ __('About the game', [], $locale.'-plus') }}</h1>
+            <p class="fs-5">{{ __('Endless Survival is a single player survival shooter game (multiplayer option is planned in the future). During the game, the player can choose a primary and a secondary weapon before each level. Players receive experience points after each course. If enough experience points are collected, the player levels up and receives a skill point. Skill points can be spent off-course on various character power-ups or abilities. In addition to skill points, new weapons become available when certain levels are reached.', [], $locale.'-plus') }}</p>
+            <p class="fs-5">{{ __('The game is a desktop application written in C#, running under the Windows operating system. The game uses the Unity engine.', [], $locale.'-plus') }}</p>
             <br>
-            <br>
-            <p class="fs-5">Az Endless Survival egy egyszemélyes túlélő shooter játék (jövőben többjátékos opció is tervben van). A játék során a játékos minden pálya előtt kiválaszthat egy elsődleges és egy másodlagos fegyvert. A játékosok minden pálya után tapasztalati pontot kapnak elegendő tapasztalati pont összegyűjtése esetén a játékos szintet lép és kap egy képesség pontot. A képesség pontokat pályán kívül lehet elkölteni különféle karakter erősítésekre vagy képességekre. Képesség pontok mellé bizonyos szintek elérésekor új fegyverek is elérhetővé válnak.
-                A játék célja, hogy a játékos minél tovább próbáljon meg túlélni a folyamatosan erősödő és egyre gyorsabban termőellenségekkel szemben.
-                </p>
-            <p style="text-align:center;"><a target="_blank" href="https://github.com/MikesDavid/Game">Letöltés</a></p>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
+            <p class="fs-5">{{ __('Endless Survival is a single player survival shooter game (multiplayer option is planned in the future). During the game, the player can choose a primary and a secondary weapon before each level. Players receive experience points after each course. If enough experience points are collected, the player levels up and receives a skill point. Skill points can be spent off-course on various character power-ups or abilities. In addition to skill points, new weapons become available when certain levels are reached. The goal of the game is for the player to try to survive as long as possible against enemies that are constantly getting stronger and spawning faster and faster.', [], $locale.'-plus') }}</p>
+            <p class="text-center"><a class="btn btn-success my-btn-success text-white fw-bold" href="https://github.com/MikesDavid/Game" target="_blank">{{ __('Download', [], $locale.'-plus') }}</a></p>
         </div>
 
 @endsection
